2ddc2905cc5a0e25110f1991f383a8f1d00613dc
clubbe <zai@Carel-LP.ZAILAB.COM>
2017-06-23 11:26:00 +0200

    feat(conf): refresh conf on file change

--- src/conf.js
+++ src/conf.js
@@ -1,6 +1,9 @@
+import limit from 'limit-framework';
 import fs from 'fs';
 import homedir from 'homedir';
 import path from 'path';
+
+const LOG = limit.Logger.get('Conf');
 
 const dir = path.join(homedir(), '.wpdivbek');
 const file = path.join(homedir(), '.wpdivbek', 's3.conf');
@@ -11,22 +14,25 @@ export class Conf {
 
     //This looks if the file is created and if not then creates the file in the users home dir
     if (fs.existsSync(dir) === false) {
-        console.log('Creating dir');
-        fs.mkdirSync(dir);
+      console.log('Creating dir');
+      fs.mkdirSync(dir);
     }
     // This will create a config file where you will put in your AWS credentials
     if (fs.existsSync(file) === false) {
-        console.log('Creating config file');
+      console.log('Creating config file');
       fs.writeFileSync(file, 'id=your_AWS_accessKeyId\r\nkey=your_AWS_secretAccessKey');
     }
-    
-    let data = fs.readFileSync(file, { encoding: 'utf8' });
-    let rows = data.split('\r\n');
-    let conf = {};
-    for (let i = 0; i < rows.length; i++) {
-      let row = rows[i];
-      let props = row.split('=');
-      this[props[0]] = props[1];
-    }
+
+    fs.watchFile(file, (curr, prev) => {
+      let data = fs.readFileSync(file, { encoding: 'utf8' });
+      let rows = data.split('\r\n');
+      let conf = {};
+      for (let i = 0; i < rows.length; i++) {
+        let row = rows[i];
+        let props = row.split('=');
+        this[props[0]] = props[1];
+      }
+      LOG.info(`${file} updated > conf = ${JSON.stringify(this, null, 2)}`);
+    });
   }
 }
--- src/sync.js
+++ src/sync.js
@@ -3,47 +3,29 @@ import s3 from 's3';
 import homedir from 'homedir';
 import fs from 'fs';
 import path from 'path';
-import {Conf} from './conf';
+import { Conf } from './conf';
 
 const LOG = limit.Logger.get('Sync');
 
 let conf = new Conf();
 
 const client = s3.createClient({
-    maxAsyncS3: 20,     // this is the default 
-    s3RetryCount: 3,    // this is the default 
-    s3RetryDelay: 1000, // this is the default 
-    multipartUploadThreshold: 20971520, // this is the default (20 MB) 
-    multipartUploadSize: 15728640, // this is the default (15 MB) 
     s3Options: {
-//Here the data is being pulled from the conf file        
         accessKeyId: conf.id,
-        secretAccessKey: conf.key,   
-        // any other options are passed to new AWS.S3() 
-        // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property 
+        secretAccessKey: conf.key
     },
 });
 
 export class Sync {
 
     static backup(backup) {
-
-        LOG.info('backup > ', backup);
-
         let folders = backup.files;
-
         for (let folder of folders) {
-
             let params = {
                 localDir: folder.absolutePath,
-                deleteRemoved: false, // default false, whether to remove s3 objects 
-                // that have no corresponding local file. 
-
                 s3Params: {
                     Bucket: "zailabbackups",
-                    Prefix: "20170524/",
-                    // other options supported by putObject, except Body and ContentLength. 
-                    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property 
+                    Prefix: "20170524/"
                 },
             };
             let uploader = client.uploadDir(params);
@@ -58,19 +40,13 @@ export class Sync {
             });
         }
     }
-//When restoring from s3 you have to chose a file (for some reason it won't restore to desktop you have to make a file on the desktop for it to work).
+
     static restore(folder) {
-        LOG.info('restore > ', folder);
         let params = {
             localDir: folder,
-            deleteRemoved: false, // default false, whether to remove s3 objects 
-            // that have no corresponding local file. 
-
             s3Params: {
                 Bucket: "zailabbackups",
-                Prefix: "20170524/",
-                // other options supported by putObject, except Body and ContentLength. 
-                // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property 
+                Prefix: "20170524/"
             },
         };
         let downloader = client.downloadDir(params);
